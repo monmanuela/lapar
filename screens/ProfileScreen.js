9883163ca7a384af3dbfaea806502bf4e844b74e
monmanuela <manuela.monika97@gmail.com>
2018-06-15 14:45:51 +0800

    added more details in sign up frontend

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'react-native-firebase';
 
 class ProfileScreen extends React.Component {
-	render() {
+  state = { currentUser: null }
+
+  componentDidMount() {
+    const currentUser = firebase.auth().currentUser;
+    this.setState({ currentUser })  
+  }
+
+  render() {
+    const currentUser = this.state.currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+
+    if (currentUser != null) {
+      name = currentUser.displayName;
+      email = currentUser.email;
+      photoUrl = currentUser.photoURL;
+      emailVerified = currentUser.emailVerified;
+      uid = currentUser.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                       // this value to authenticate with your backend server, if
+                       // you have one. Use User.getToken() instead.
+    }
+
 		return (
 			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Profile!</Text>
+        <Text>Hi {currentUser && name} !</Text>
+        <Text>Email: {currentUser && email}</Text>
+        <Text>photoUrl: {currentUser && photoUrl}</Text>
+        <Text>emailVerified: {currentUser && emailVerified}</Text>
+        <Text>uid: {currentUser && uid}</Text>
+
       </View>
 		);
 	}
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -4,28 +4,59 @@ import firebase from 'react-native-firebase'
 
 export default class SignUp extends React.Component {
   state = {
+    displayName: '',
+    username: '',
     email: '',
     password: '',
     errorMessage: null
   }
 
   handleSignUp = () => {
+    const db = firebase.database()
+
     firebase
       .auth()
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then(() => this.props.navigation.navigate('Home'))
       .catch(error => this.setState({ errorMessage: error.message }))
-    // console.log('handleSignUp')
+
+    db.ref('users/' + userId).set({
+      displayName: this.state.displayName,
+      username: this.state.username,
+      email: this.state.email
+    })
+    // later add profile picture
+    // store the rest of the data in the database
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Sign Up</Text>
+
         {this.state.errorMessage &&
           <Text style={{ color: 'red' }}>
             {this.state.errorMessage}
           </Text>}
+
+        {/* Display name */}
+        <TextInput
+          placeholder="Display name"
+          style={styles.textInput}
+          onChangeText={displayName => this.setState({ displayName })}
+          value={this.state.displayName}
+        />
+
+        {/* Username */}
+        <TextInput
+          placeholder="Username"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={username => this.setState({ username })}
+          value={this.state.username}
+        />
+
+        {/* Email */}
         <TextInput
           placeholder="Email"
           autoCapitalize="none"
@@ -33,6 +64,8 @@ export default class SignUp extends React.Component {
           onChangeText={email => this.setState({ email })}
           value={this.state.email}
         />
+
+        {/* Password */}
         <TextInput
           secureTextEntry
           placeholder="Password"
@@ -41,7 +74,12 @@ export default class SignUp extends React.Component {
           onChangeText={password => this.setState({ password })}
           value={this.state.password}
         />
-        <Button title="Sign Up" onPress={this.handleSignUp} />
+
+        <Button 
+          title="Sign Up"
+          onPress={this.handleSignUp}  
+        />
+
         <Button
           title="Already have an account? Login"
           onPress={() => this.props.navigation.navigate('Login')}
