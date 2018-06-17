1e721f8569ffca704f05d217fb443a6e6252e763
monmanuela <manuela.monika97@gmail.com>
2018-06-17 13:29:18 +0800

    moved displayname and photourl from firebase database to firebase authentication, added user profile photo

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
+import { Text, View, TextInput, Button, StyleSheet, Image } from 'react-native';
 import firebase from 'react-native-firebase';
 import EditProfileScreen from './EditProfileScreen'
 
@@ -21,17 +21,25 @@ export default class ProfileScreen extends React.Component {
   componentDidMount() {
     // show loading swirling logo
     const currentUser = firebase.auth().currentUser;
+    console.log("currentUser: " + JSON.stringify(currentUser))
     
     if (currentUser != null) {
       var db = firebase.database()
+      this.setCurrentUser(currentUser)
 
       db.ref("users").orderByKey().equalTo(currentUser.uid).once("value").then(function(snapshot) {
         console.log("snapshot: " + JSON.stringify(snapshot))
         var key = currentUser.uid
         var userData = snapshot.val()[key]
         return userData
-      }).then(userData => this.setUserData(userData))
+      }).then(userData => {
+        console.log("userData: " + JSON.stringify(userData))
+        this.setUserData(userData) })
     }
+  }
+
+  setCurrentUser = currentUser => {
+    this.setState({ currentUser })
   }
 
   setUserData = userData => {
@@ -45,10 +53,16 @@ export default class ProfileScreen extends React.Component {
   render() {
 		return (
 			<View style={{ flex: 1, alignItems: 'center' }}>
-        <Text>Photo Here</Text>
-        <Text>{this.state.userData.displayName}</Text>
+        {this.state.currentUser &&
+          <Image
+            style={{width: 150, height: 150}}
+            source={{uri: this.state.currentUser.photoURL}}
+          />
+        }
+
+        <Text>{this.state.currentUser && this.state.currentUser.displayName}</Text>
         <Text>Bio Here</Text>
-        <Text>{this.state.userData.bio}</Text>
+        <Text>{this.state.userData && this.state.userData.bio}</Text>
         <Button title="Edit Profile" onPress={this.handleEditProfile} />
         {/* Bio, Badges, Last Activities/Previous Reviews*/}
         {/* Store preferences in userData */}
